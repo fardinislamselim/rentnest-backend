e71f89ded63e15027842af3d0401a6587821a11c
fardinislamselim <codewithfardin@gmail.com>
2026-07-06 14:01:15 +0600

    add configuration loading from .env

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
 import express, { Application, Request, Response } from "express";
 import httpStatus from "http-status";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import config from "./config";
+
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.status(httpStatus.OK).json({ message: "RentNest Server is running" });
