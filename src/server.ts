43f7ab0b9822d2b22b568c517a4b3332cde5a4d2
fardinislamselim <codewithfardin@gmail.com>
2026-07-07 14:11:23 +0600

    integrate Prisma with PostgreSQL and set up database connection

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
-import app from "./app.js";
+import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
 
-const port = 5000;
+const port = config.port
 
 async function main() {
   try {
+    await prisma.$connect();
     console.log("Database connected");
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
     console.error("Error starting the server:", error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
